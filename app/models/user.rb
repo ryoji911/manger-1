21e7cacc8ed1c034696c79d9e7b94bd1cc3a3e7a
ryoji911 <ryoji407_1109@yahoo.co.jp>
2020-12-25 18:40:26 +0900

    修正１

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   has_many :recipes, dependent: :destroy
-  has_many :comments, dependent: :destroy
+  has_many :comments
   has_one_attached :image
   has_many :likes, dependent: :destroy
   has_many :liked_recipes, through: :likes, source: :recipe
