d48e65298904ca3fa86fc41a7258105320fa6cfb
ryoji911 <ryoji407_1109@yahoo.co.jp>
2020-12-28 12:00:54 +0900

    AWSでの画像投稿の実装

--- Gemfile
+++ Gemfile
@@ -63,3 +63,5 @@ gem 'mini_magick'
 gem 'image_processing', '~> 1.2'
 
 gem 'kaminari'
+
+gem "aws-sdk-s3", require: false
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,6 @@
 class Recipe < ApplicationRecord
   belongs_to :user
-  has_many :comments
+  has_many :comments, dependent: :destroy
   has_one_attached :image
   has_many :likes, dependent: :destroy
   has_many :liked_users, through: :likes, source: :user
